f2da31bf09e157c5408e4249203d86ff279bc528
NhatMy <miketran1406@gmail.com>
2023-08-13 15:47:29 +0700

    newheader

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Headers from './components/header'
+
+
+function App() {
+  return (
+    <div>
+      <h1>APC Website</h1>
+      <Headers/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function header() {
+  return (
+    <div>
+        <ul>
+            <li> NFT marrket</li>
+            <li> About us</li>
+            <li> my team</li>
+        </ul>
+        <button> Connect Wallet</button> <br></br>
+        <button>Create NFT</button>
+    </div>
+  )
+}
